f8cd82a73aabb3ad3dfbe4031cdc385d1a922a15
Anjali G <anjali.g@cabot.loc>
2021-07-19 18:34:52 +0530

    first commit

---
+++ arithmeticcalc/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace arithmeticcalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select the action to be performed");
+            Console.WriteLine("1-Addition 2-Subtraction 3-Multiplication 4-Division");
+            int operation = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter first number");
+            var num1 = Convert.ToDecimal(Console.ReadLine());
+            var num2 = Convert.ToDecimal(Console.ReadLine());
+            decimal result = 0;
+            switch (operation)
+            {
+                case 1:
+                    {
+                        result = (num1 + num2);
+                        break;
+                    }
+                case 2:
+                    {
+                        result = (num1 - num2);
+                        break;
+                    }
+                case 3:
+                    {
+                        result = (num1 * num2);
+                        break;
+                    }
+                case 4:
+                    {
+                        result = (num1 / num2);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Wrong Selection");
+                        break;
+                    }
+            }
+            Console.WriteLine("Result = {0} ", result);
+        }
+    }
+}
